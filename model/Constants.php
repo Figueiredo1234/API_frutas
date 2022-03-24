a062a5f43de479c4f53fe9a3c84a43b711dbc1df
Figueiredo1234 <86685363+Figueiredo1234@users.noreply.github.com>
2022-03-24 17:16:44 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,6 +1,6 @@
 <?php
 
-define ('DB_HOST','localhost');
-define ('DB_USER','root');
-define ('DB_PASS','');
+define ('DB_HOST','victorfigueiredo.mysql.database.azure.com');
+define ('DB_USER','victorpfsilva@victorfigueiredo');
+define ('DB_PASS','salmos27#');
 define ('DB_NAME','frutas_tb');
